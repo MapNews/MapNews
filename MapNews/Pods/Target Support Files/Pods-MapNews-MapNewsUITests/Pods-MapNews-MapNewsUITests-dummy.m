b3122b00dbf10cc72ac2f258f1db4f5f34858e42
hoholyin <holyin97@gmail.com>
2020-05-18 09:17:58 +0800

    Add map view with SG as center

---
+++ MapNews/Pods/Target Support Files/Pods-MapNews-MapNewsUITests/Pods-MapNews-MapNewsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MapNews_MapNewsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MapNews_MapNewsUITests
+@end
---
+++ MapNews/Pods/Target Support Files/Pods-MapNewsTests/Pods-MapNewsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MapNewsTests : NSObject
+@end
+@implementation PodsDummy_Pods_MapNewsTests
+@end
