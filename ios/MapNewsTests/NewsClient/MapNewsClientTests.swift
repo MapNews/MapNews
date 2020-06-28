36e33141fcfd2afc158a68d7b3c8d9be0d4fe872
hoholyin <holyin97@gmail.com>
2020-06-28 23:08:03 +0800

    Add news client tests

--- ios/MapNewsTests/NewsClient/MapNewsClientTests.swift
+++ ios/MapNewsTests/NewsClient/MapNewsClientTests.swift
@@ -12,25 +12,41 @@ class MapNewsClientTests: XCTestCase {
     var retrieveResultsExpectation: XCTestExpectation!
     var newsClient = MapNewsClient()
 
-    func testQueryData_countryCodeIsValid() {
+    override func setUp() {
         retrieveResultsExpectation = expectation(description: "results retrieved")
+    }
+
+    func testQueryData_countryCodeIsValid() {
         newsClient.queryData(countryCode: "SG", callback: fulfillExpectationCallback(_:))
         waitForExpectations(timeout: 10, handler: nil)
     }
 
+    func testQueryData_countryCodeIsInvalid() {
+        newsClient.queryData(countryCode: "HW", callback: noResultsCallback(_:))
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+
+    func testQueryData_urlIsInvalid() {
+        retrieveResultsExpectation.isInverted = true
+        newsClient.queryData(countryCode: "://\\\\.,&8", callback: noResultsCallback(_:))
+        waitForExpectations(timeout: 10, handler: nil)
+    }
+
     func testQueryData_jsonFormatIsValid() {
-        retrieveResultsExpectation = expectation(description: "results retrieved")
         newsClient.queryData(countryCode: "SG", callback: createArticleDTOCallback(_:))
         waitForExpectations(timeout: 10, handler: nil)
     }
 
-    func testQueryArticles() {
-        retrieveResultsExpectation = expectation(description: "results received")
+    func testQueryArticles_validCountryCode() {
         newsClient.queryArticles(
             country: CountryCoordinateDTO(name: "Canada", countryCode: "CA", coordinates:
                 Coordinates(lat: 0.5, long: 0.9)!),
             callback: returnArticlesCallback(_:_:))
         waitForExpectations(timeout: 30, handler: nil)
+    }
+
+    func testDownloadImage_invalidUrl() {
+        
     }
 }
 
@@ -45,6 +61,19 @@ extension MapNewsClientTests {
             return
         }
         XCTAssertNotEqual(articles.count, 0)
+        retrieveResultsExpectation.fulfill()
+    }
+
+    func noResultsCallback(_ result: Data) {
+        guard let jsonObject = JSONParser.createJsonObject(from: result) else {
+            XCTFail("Unable to parse to json object")
+            return
+        }
+        guard let articles = JSONParser.getArray(from: jsonObject, key: "articles") else {
+            XCTFail("Should be able to cast articles as an array")
+            return
+        }
+        XCTAssertEqual(articles.count, 0)
         retrieveResultsExpectation.fulfill()
     }
 
