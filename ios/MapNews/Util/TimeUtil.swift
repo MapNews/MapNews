f4760515b8e25bbfbeda87af5953f3a7489ac9e5
hoholyin <holyin97@gmail.com>
2020-08-04 22:24:17 +0800

    Refactor constants

--- ios/MapNews/Util/TimeUtil.swift
+++ ios/MapNews/Util/TimeUtil.swift
@@ -9,25 +9,25 @@ import Foundation
 
 struct TimeUtil {
     static let DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
+    private static let SECONDS_IN_DAY: Double = 86400
+    private static let SECONDS_IN_HOUR: Double = 3600
+    private static let SECONDS_IN_MIN: Double = 60
 
     static func dateToTimeDisplayString(date: Date?) -> String {
         guard let dateObj = date else {
             return ""
         }
-        let dayInterval: Double = 86400
-        let hourInterval: Double = 3600
-        let minInterval: Double = 60
         let publishedInterval = dateObj.distance(to: Date())
-        if publishedInterval < hourInterval {
-            let minutesAgo = Int(floor(publishedInterval / minInterval))
+        if publishedInterval < SECONDS_IN_HOUR {
+            let minutesAgo = Int(floor(publishedInterval / SECONDS_IN_MIN))
             let unit = minutesAgo == 1 ? "min" : "mins"
             return "\(minutesAgo) \(unit) ago"
-        } else if publishedInterval < dayInterval {
-            let hoursAgo = Int(floor(publishedInterval / hourInterval))
+        } else if publishedInterval < SECONDS_IN_DAY {
+            let hoursAgo = Int(floor(publishedInterval / SECONDS_IN_HOUR))
             let unit = hoursAgo == 1 ? "hour" : "hours"
             return "\(hoursAgo) \(unit) ago"
         } else {
-            let daysAgo = Int(floor(publishedInterval / dayInterval))
+            let daysAgo = Int(floor(publishedInterval / SECONDS_IN_DAY))
             let unit = daysAgo == 1 ? "day" : "days"
             return "\(daysAgo) \(unit) ago"
         }
